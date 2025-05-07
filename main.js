fdc654e31a714361a39c4d94ee7c6af31948a5ce
Yasmin <146743272+iasminzoca@users.noreply.github.com>
2025-05-07 11:10:58 -0300

    jsdnjs

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:10");
 const tempoObjetivo2 = new Date("2025-12-05T00:00:10");
 const tempoObjetivo3 = new Date("2025-12-30T00:00:10");
-const tempoObjetivo4 = new Date("2025-02-01T00:00:10");
+const tempoObjetivo4 = new Date("2025-012-01T00:00:10");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
